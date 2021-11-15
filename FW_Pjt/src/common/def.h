4f973031f68386071f9758e9c35cce32e1c9de4c
EunSikChoi <chldmstlr79@naver.com>
2021-11-15 14:08:54 +0900

    LED_초기 프로젝트 생성 및  LED 토글 구현 완료

---
+++ FW_Pjt/src/common/def.h
@@ -0,0 +1,15 @@
+/*
+ * def.h
+ *
+ *  Created on: Nov 15, 2021
+ *      Author: 82109
+ */
+
+#ifndef SRC_COMMON_DEF_H_
+#define SRC_COMMON_DEF_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif /* SRC_COMMON_DEF_H_ */
---
+++ FW_Pjt/src/hw/hw_def.h
@@ -0,0 +1,20 @@
+/*
+ * hw_def.h
+ *
+ *  Created on: Nov 15, 2021
+ *      Author: 82109
+ */
+
+#ifndef SRC_HW_HW_DEF_H_
+#define SRC_HW_HW_DEF_H_
+
+
+
+
+#include "def.h"
+#include "bsp.h"
+
+
+
+
+#endif /* SRC_HW_HW_DEF_H_ */
